026d29e4fce4707a791217adfefaf50dabd89dfa
Deepak kumar <srphyvlab@gmail.com>
2025-10-28 08:55:04 +0530

    feat(chat): optimistic UI for sending messages (temp messages, sending indicator)

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,7 +2,9 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma = globalForPrisma.prisma || new PrismaClient({
+  log: ['error', 'warn'],
+});
 
 export default prisma;
 
